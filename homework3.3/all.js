c1b688187451802d7019bbd2a2ec819165c25aa1
Stupak Serhii <stupak.s.v1337@gmail.com>
2025-07-22 21:30:32 +0300

    fix_homework3.3

--- homework3.3/all.js
+++ homework3.3/all.js
@@ -1,6 +1,18 @@
-let value1 = '1';
-let value2 = ' 0';
-let value3 = ' 3';
-let value4 = ' 6';
-let value5 = ' 9';
-console.log(value1 + value2 + value3 + value4 + value5);
+let number = 10369;
+
+let digit1 = number - (number % 10000);
+digit1 = digit1 / 10000;
+
+let digit2 = (number % 10000) - (number % 1000);
+digit2 = digit2 / 1000;
+
+let digit3 = (number % 1000) - (number % 100);
+digit3 = digit3 / 100;
+
+let digit4 = (number % 100) - (number % 10);
+digit4 = digit4 / 10;
+
+let digit5 = number % 10;
+
+console.log(digit1, digit2, digit3, digit4, digit5);
+
